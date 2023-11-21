bad32533d44c41029bf870e898ed3edfc822a02a
Truong Le <21521601@gm.uit.edu.vn>
2023-11-22 01:32:36 +0700

    add export order to pdf

--- react/src/pages/Admin/test_OrderManage/orderManage.js
+++ react/src/pages/Admin/test_OrderManage/orderManage.js
@@ -1,25 +1,35 @@
 import "./orderManage.css"
 import 'bootstrap';
 import request from "../../../utils/request";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock, faFaceAngry } from '@fortawesome/free-regular-svg-icons';
 import {  faL, faUser, faXmark } from '@fortawesome/free-solid-svg-icons';
 
-import { Document, Page, Text, View, StyleSheet, PDFDownloadLink } from '@react-pdf-viewer/core';
-import { printPlugin } from '@react-pdf-viewer/print';
+import { useReactToPrint } from "react-to-print";
 
-const handleClickImportOrder = () => {
-    alert("test_Coi như cái này là hóa đơn nha!!!!!!!!!")
-}
 
 function OrderManage() {
+
+    const componentPDF = useRef();
+
+    const handleClickImportOrder = useReactToPrint({
+        content:() => componentPDF.current,
+        documentTitle:"Order",
+        onAfterPrint:() => alert("Đã xuất hóa đơn")
+    });
+
     return (
         <div>
+            <div ref={componentPDF}>
+                <p>Chi tiết hóa đơn</p>
+                <p>Danh sách sản phẩm</p>
+                <p>Tổng tiền: 25000</p>
+            </div>
             <button class="btn btn-primary" onClick={handleClickImportOrder}>Xuất hóa đơn</button>
         </div>
-    )
+    );
 }
 
 export default OrderManage;
