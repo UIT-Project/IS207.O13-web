e8246066aa1544c96473549369cb6a3aa07db667
dosidat1503 <dosidat15031712@gmail.com>
2023-11-22 10:49:15 +0700

    ok

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,20 @@
 <?php
 
-use App\Http\Controllers\PayOnline;
-use App\Http\Controllers\PayOnlineController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ManageProductController; 
+use App\Http\Controllers\PayOnlineController;
+use App\Http\Controllers\AddProductController;
+use App\Http\Controllers\AdminManageOrderController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InfoProductController;
+use App\Http\Controllers\PaymentController;
+ 
+use App\Http\Controllers\SearchProductController;
+use App\Http\Controllers\UpdateProductController;
+use App\Http\Controllers\LoginController;
 
 
 /*
@@ -22,52 +32,52 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 //Home
-Route::get('getInfoAtStartLoadingHome', [TestController::class, 'getInfoAtStartLoadingHome']);
+Route::get('getInfoAtStartLoadingHome', [HomeController::class, 'getInfoAtStartLoadingHome']);
 
 //Login
-Route::post('register', [TestController::class, 'register']);
-Route::post('login', [TestController::class, 'login']);
+Route::post('register', [LoginController::class, 'register']);
+Route::post('login', [LoginController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function() {
-    Route::post('logout', [TestController::class, 'logout']); 
+    Route::post('logout', [LoginController::class, 'logout']); 
 });
 
 //Search Product
-Route::get('search', [TestController::class, 'search']);
-Route::get('filterSearchProduct', [TestController::class, 'filterSearchProduct']);
+Route::get('search', [SearchProductController::class, 'search']);
+Route::get('filterSearchProduct', [SearchProductController::class, 'filterSearchProduct']);
 
 
 //InfoProduct
-Route::get('infoProduct', [TestController::class, 'infoProduct']);
-Route::post('addToCart', [TestController::class, 'addToCart']);
+Route::get('infoProduct', [InfoProductController::class, 'infoProduct']);
+Route::post('addToCart', [InfoProductController::class, 'addToCart']);
 // Route::get('infoPopupCart', [TestController::class, 'infoPopupCart']);
-Route::post('updateQuantityProductInCart', [TestController::class, 'updateQuantityProductInCart']);
+Route::post('updateQuantityProductInCart', [InfoProductController::class, 'updateQuantityProductInCart']);
 
 //Cart
-Route::get('infoCart', [TestController::class, 'infoCart']);
-Route::post('updateSelectedProperty', [TestController::class, 'updateSelectedProperty']);
-Route::post('deleteItemCart', [TestController::class, 'deleteItemCart']);
-Route::post('updateQuantityProperty', [TestController::class, 'updateQuantityProperty']);
+Route::get('infoCart', [CartController::class, 'infoCart']);
+Route::post('updateSelectedProperty', [CartController::class, 'updateSelectedProperty']);
+Route::post('deleteItemCart', [CartController::class, 'deleteItemCart']);
+Route::post('updateQuantityProperty', [CartController::class, 'updateQuantityProperty']);
 
 //Payment + Payment Online VNPAY
-Route::get('infoForPayment', [TestController::class, 'infoForPayment']);
-Route::post('saveInfoForPayment', [TestController::class, 'saveInfoForPayment']);
+Route::get('infoForPayment', [PaymentController::class, 'infoForPayment']);
+Route::post('saveInfoForPayment', [PaymentController::class, 'saveInfoForPayment']);
 Route::post('payOnline', [PayOnlineController::class, 'payOnline']);
 
 //Payment Result
-Route::post('processPaymentResult', [TestController::class, 'processPaymentResult']);
+Route::post('processPaymentResult', [PaymentController::class, 'processPaymentResult']);
 
 //Admin - AddProduct
-Route::post('addProduct', [TestController::class, 'addProduct']);
-Route::get('getInfoForAddProduct', [TestController::class, 'getInfoForAddProduct']);
-Route::get('linkImageProduct', [TestController::class, 'linkImageProduct']);
+Route::post('addProduct', [AddProductController::class, 'addProduct']);
+Route::get('getInfoForAddProduct', [AddProductController::class, 'getInfoForAddProduct']);
+Route::get('linkImageProduct', [AddProductController::class, 'linkImageProduct']);
 
 
 
 //Admin - ManageOrder
-Route::get('getQuantityOrderToDevidePage', [TestController::class, 'getQuantityOrderToDevidePage']);
-Route::get('getInfoManageOrder', [TestController::class, 'getInfoManageOrder']);
-Route::get('infoOrderDetail', [TestController::class, 'infoOrderDetail']);
+Route::get('getQuantityOrderToDevidePage', [AdminManageOrderController::class, 'getQuantityOrderToDevidePage']);
+Route::get('getInfoManageOrder', [AdminManageOrderController::class, 'getInfoManageOrder']);
+Route::get('infoOrderDetail', [AdminManageOrderController::class, 'infoOrderDetail']);
 
 
 Route::get('testtest', [TestController::class, 'testtest']);
@@ -75,10 +85,10 @@ Route::get('testlaythongtin', [TestController::class, 'testlaythongtin']);
 
 
 //Admin - ManageProduct
-Route::get('getQuantityProductToDevidePage', [TestController::class, 'getQuantityProductToDevidePage']);
-Route::get('getInfoManageProduct', [TestController::class, 'getInfoManageProduct']);
-Route::get('infoProductDetail', [TestController::class, 'infoProductDetail']);
+Route::get('getQuantityProductToDevidePage', [ManageProductController::class, 'getQuantityProductToDevidePage']);
+Route::get('getInfoManageProduct', [ManageProductController::class, 'getInfoManageProduct']);
+Route::get('infoProductDetail', [ManageProductController::class, 'infoProductDetail']);
 
 //Admin - UpdateProduct
-Route::post('updateProduct', [TestController::class, 'updateProduct']);
-Route::post('updateProduct2', [TestController::class, 'updateProduct2']);
+Route::post('updateProduct', [UpdateProductController::class, 'updateProduct']);
+Route::post('updateProduct2', [UpdateProductController::class, 'updateProduct2']);
